45a2e5a4f9f1086da13939e1449786948f35dafb
Deepthi <deepthi.gaddipati.f2jn@statefarm.com>
2021-09-17 16:11:35 -0500

    Added some customized annotations

---
+++ src/main/java/customAnnotations/ObjectToJsonConverter.java
@@ -0,0 +1,13 @@
+package customAnnotations;
+
+public class ObjectToJsonConverter {
+    public String convertToJson(Object object) throws JsonSerializationException {
+        try {
+            checkIfSerializable(object);
+            initializeObject(object);
+            return getJsonString(object);
+        } catch (Exception e) {
+            throw new JsonSerializationException(e.getMessage());
+        }
+    }
+}
